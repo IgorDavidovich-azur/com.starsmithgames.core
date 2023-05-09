81468decd3a409ed2f6d00712ab5d453357a15f8
Davidovich Igor Alexandrovich <sir.funtik18@gmail.com>
2023-05-09 11:54:34 +0700

    Create Base Package

---
+++ Runtime/Extensions/StringExtensions.cs
@@ -0,0 +1,13 @@
+namespace StartSmithGames.Core
+{
+	public static class StringExtensions
+	{
+		/// <summary>
+		/// Checks if a string is empty.
+		/// </summary>
+		public static bool IsEmpty(this string s)
+		{
+			return string.IsNullOrEmpty(s) || string.IsNullOrWhiteSpace(s);
+		}
+	}
+}
